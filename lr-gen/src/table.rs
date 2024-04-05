b998b18cba96035d9f86a3442e967b5c43dae84c
Maix0 <maix522@gmail.com>
2024-04-05 22:09:15 +0200

    updated

--- lr-gen/src/table.rs
+++ lr-gen/src/table.rs
@@ -198,21 +198,21 @@ print reductions
 fn test(rules: &[LR1Item]) {
     use std::rc::Rc;
     let mut itemsets: Vec<Rc<[DottedRule]>> = vec![vec![DottedRule::new(0, 0)].into()];
-    let itemsets_index = itemsets
+    let mut itemsets_index = itemsets
         .iter()
         .enumerate()
         .map(|(i, a)| (a.clone(), i))
         .collect::<HashMap<_, _>>();
     let mut vectors = Vec::new();
-    let mut full_itemsets = Vec::new();
+    let mut full_itemsets = Vec::<Rc<[_]>>::new();
     let mut shifts = Vec::new();
     let mut reductions = Vec::new();
     let mut k = 0;
     while k < itemsets.len() {
         let itemset = &itemsets[k];
-        vectors.push(itemset);
+        vectors.push(itemset.clone());
         let pset = predict(rules, &itemset);
-        full_itemsets.push(pset);
+        full_itemsets.push(pset.clone().into());
         print_itemset(k, rules, itemset);
         let mut k_shifts = HashMap::new();
         let mut k_reductions = HashSet::new();
@@ -222,8 +222,132 @@ fn test(rules: &[LR1Item]) {
                     k_reductions.insert(i);
                 }
             } else {
+                let citems: Rc<_> = items.clone().into();
+                let j = itemsets_index.get(&citems).copied().unwrap_or_else(|| {
+                    let ret = itemsets.len();
+                    itemsets_index.insert(citems.clone(), ret);
+                    itemsets.push(citems.clone());
+                    ret
+                });
+                k_shifts.insert(sym, j);
             }
         }
+        shifts.push(k_shifts);
+        reductions.push(k_reductions);
         k += 1;
+    }
+}
+/*
+def empty_symbols():
+    symbols = set()
+    for lhs,rhs in grammar:
+        if len(rhs) == 0:
+            symbols.add(lhs)
+    m = 0
+    n = len(symbols)
+    while m < n:
+        for lhs, rhs in grammar:
+            if all(x in symbols for x in rhs):
+                symbols.add(lhs)
+        m = n
+        n = len(symbols)
+    return symbols
+empty = empty_symbols()
+*/
+
+fn empty_symbols(rules: &[LR1Item]) -> HashSet<crate::Crc<str>> {
+    let mut symbols = HashSet::new();
+    for LR1Item { lhs, rhs, .. } in rules {
+        if rhs.is_empty() {
+            symbols.insert(lhs.clone());
+        }
+    }
+    let mut m = 0;
+    let mut n = symbols.len();
+    while m < n {
+        for LR1Item { lhs, rhs, .. } in rules {
+            if rhs.iter().all(|x| match x {
+                LR1Token::NonTerminal(n) => symbols.contains(n),
+                LR1Token::EndOfInput => symbols.contains("$"),
+                LR1Token::Terminal(_) => symbols.contains("__char_builtin__"),
+            }) {
+                symbols.insert(lhs.clone());
+            }
+        }
+        m = n;
+        n = symbols.len();
+    }
+    symbols
+}
+
+/*
+def first_lexemes():
+    symbols = dict()
+    routes = set()
+    for sym in lexemes:
+        symbols[sym] = set([sym])
+    for lhs, rhs in grammar:
+        if lhs not in symbols:
+            symbols[lhs] = set([])
+    for lhs, rhs in grammar:
+        for rhsN in rhs:
+            routes.add((lhs,rhsN))
+            if rhsN not in empty:
+                break
+    rep = True
+    while rep:
+        rep = False
+        for lhs, rhs0 in routes:
+            n = len(symbols[lhs])
+            symbols[lhs].update(symbols[rhs0])
+            rep |= n < len(symbols[lhs])
+    return symbols
+
+first = first_lexemes()
+print first
+*/
+
+fn first_lexemes(empty: &HashSet<crate::Crc<str>>, lexemes: &[crate::Crc<str>], rules: &[LR1Item]) {
+    let mut symboles = HashMap::new();
+    let mut routes = HashSet::new();
+
+    for sym in lexemes {
+        symboles.insert(sym.clone(), HashSet::from([sym.clone()]));
+    }
+    for LR1Item { lhs, rhs, .. } in rules {
+        if !symboles.contains_key(lhs) {
+            symboles.insert(lhs.clone(), HashSet::new());
+        }
+    }
+    for LR1Item { lhs, rhs, .. } in rules {
+        for rhs_n in rhs {
+            routes.insert((lhs.clone(), rhs_n.clone()));
+            let s = match rhs_n {
+                LR1Token::NonTerminal(n) => &n,
+                LR1Token::EndOfInput => "$",
+                LR1Token::Terminal(_) => "__char_builtin__",
+            };
+            if !empty.contains(s) {
+                break;
+            }
+        }
+    }
+    let mut rep = true;
+    while rep {
+        rep = false;
+
+        for (lhs, rhs0) in &routes {
+            let n = symboles.get(lhs).map(|s| s.len()).unwrap_or(0);
+            let add = symboles[match rhs0 {
+                LR1Token::NonTerminal(n) => &n,
+                LR1Token::EndOfInput => "$",
+                LR1Token::Terminal(_) => "__char_builtin__",
+            }]
+            .iter()
+            .cloned()
+            .collect::<Vec<_>>();
+            symboles.entry(lhs.clone()).or_default().extend(add);
+            rep |= n < symboles[lhs].len();
+        }
     }
 }
